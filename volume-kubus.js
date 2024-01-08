342bff36fe62b567c2a4c00392489e516f09f93b
NovemArdan <novemrohmadin@gmail.com>
2024-01-08 14:53:23 +0700

    file volume kubus

---
+++ volume-kubus.js
@@ -0,0 +1,8 @@
+const formulaVolumeKubus = (sikuSiku) => {
+    return sikuSiku * sikuSiku * sikuSiku 
+}
+
+const sikuSiku = Number(process.argv[2])
+
+//console.log(formulaVolumeKubus(sikuSiku))
+console.log(formulaVolumeKubus(sikuSiku))
